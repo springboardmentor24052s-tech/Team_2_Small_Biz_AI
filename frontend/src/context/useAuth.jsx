103b49634d969a4bca23c66c5ced15e7b66ec195
Neelam Rishika Damini <neelamrishikadamini@gmail.com>
2026-07-31 09:36:53 +0530

    Save local progress before pulling changes

--- frontend/src/context/useAuth.jsx
+++ frontend/src/context/useAuth.jsx
@@ -1,12 +1,8 @@
 import { useContext } from "react";
 import AuthContext from "./AuthContext";
 
-
-function useAuth(){
-
-    return useContext(AuthContext);
-
-}
-
+const useAuth = () => {
+  return useContext(AuthContext);
+};
 
 export default useAuth;
